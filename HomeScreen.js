5cd1acdc406cb06d7acfbc498b7806141a675100
Pogod171 <127141703+Pogod171@users.noreply.github.com>
2024-04-26 12:58:47 -0400

    Update HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,31 +1,33 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
-// import { Audio } from 'expo-av';
-// import { Camera } from "expo-camera";
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image, Alert } from 'react-native';
+import { Audio } from 'expo-av';
+import { Camera } from "expo-camera";
 import UserContext from './UserContext';
 
 const HomeScreen = ({ navigation, route }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const { userData, setUserData } = useContext(UserContext);
-    // const [permissionAudioResponse, requestPermissionAudio] = Audio.usePermissions();
-    // const [permissionPhoto, requestPermissionPhoto] = Camera.useCameraPermissions();
+    const [permissionAudioResponse, requestPermissionAudio] = Audio.usePermissions();
+    const [permissionPhoto, requestPermissionPhoto] = Camera.useCameraPermissions();
     const users = {
         'LinaJ': 'password1',
         'XavierT': 'password2',
         'admin': 'admin',
     };
     
-    // const handleRequestPermission = async () => {
-    //     const { statusPhoto} = await requestPermissionPhoto();
-    //     if (statusPhoto !== 'granted') {
-    //       Alert.alert('Permission de la caméra requise', 'Il faut votre permission afin d\'utiliser la caméra pour votre photo de profil.');
-    //     }
-    //     const { statusAudio } = await requestPermissionAudio();
-    //     if (statusAudio !== 'granted') {
-    //       Alert.alert('Permission du microhpone requise', 'Il faut votre permission afin d\'utiliser le microphone pour votre audio.');
-    //     }
-    // };
+    const handleRequestPermissionPhoto = async () => {
+        const { statusPhoto } = await requestPermissionPhoto();
+        if (statusPhoto !== 'granted') {
+          Alert.alert('Permission de la caméra requise', 'Il faut votre permission afin d\'utiliser la caméra pour votre photo de profil.');
+        }
+    };
+    const handleRequestPermissionAudio = async () => {
+        const { statusAudio } = await requestPermissionAudio();
+        if (statusAudio !== 'granted') {
+          Alert.alert('Permission du microhpone requise', 'Il faut votre permission afin d\'utiliser le microphone pour votre audio.');
+        }
+    };
 
     useEffect(() => {
         setUserData(userData);
@@ -38,7 +40,8 @@ const HomeScreen = ({ navigation, route }) => {
                 ...prevUserData,
                 username: username,
             }));
-            // handleRequestPermission();
+            handleRequestPermissionPhoto();
+            handleRequestPermissionAudio();
         } else {
             alert('Nom d\'utilisateur ou mot de passe incorrect');
         }
@@ -136,7 +139,6 @@ const styles = StyleSheet.create({
     teamMember: {
         fontSize: 18,
         color: '#333',
-        marginBottom : 15
     },
     input: {
         width: '100%',
